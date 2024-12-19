5607599275ccd274d173e74adda48f94d38681dc
SayanSTR <sayan02051996play@gmail.com>
2024-12-19 22:57:32 +0530

    update

--- lessons/part1/c/state-and-events/src/App.jsx
+++ lessons/part1/c/state-and-events/src/App.jsx
@@ -1,45 +1,60 @@
 import './App.css'
 import { useState } from 'react'
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-  console.log('rendering... ' + counter)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <Display counter={counter} />
-      <Button handler={increaseByOne} text='plus' />
-      <Button handler={setToZero} text='zero' />
-      <Button handler={decreaseByOne} text='minus' />
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const Display = ({ counter }) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+const App = () => {
+  const [clicks, setClicks] = useState({ left: 0, right: 0 })
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  console.log('App start...', clicks)
 
-const Button = (props) => {
+  const handleLeftClick = () => {
+    const updatedLeft = clicks.left + 1
+    setClicks({ ...clicks, left: updatedLeft })
+    setAll(allClicks.concat('L'))
+    setTotal(updatedLeft + clicks.right)
+    console.log('App clicks: ', clicks, 'total:', total)
+  }
+
+  const handleRightClick = () => {
+    const updatedRight = clicks.right + 1
+    setClicks({ ...clicks, right: updatedRight })
+    setAll(allClicks.concat('R'))
+    setTotal(clicks.left + updatedRight)
+    console.log('App clicks: ', clicks, 'total:', total)
+  }
+
   return (
-    <button onClick={props.handler}>
-      {props.text}
-    </button>
+    console.log('App returning...', clicks, 'total:', total),
+    <div>
+      {clicks.left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {clicks.right}
+      <p>total {total}</p>
+      <History allClicks={allClicks} />
+    </div>
   )
 }
 
